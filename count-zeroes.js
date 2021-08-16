3bc8e9dd2a27afb57317fb7dd32ab636d9d0cc05
mits <dmitrybaycharov@gmail.com>
2021-08-16 12:01:30 -0500

    sorted freq

--- count-zeroes.js
+++ count-zeroes.js
@@ -1,5 +1,21 @@
-function countZeroes() {
-  
+//binary search the first one that has a zero to the right
+function countZeroes(arr) {
+  //edge case if all zeros
+  if (arr[0] === 0) return arr.length;
+
+  let leftIdx = 0;
+  let rightIdx = arr.length - 1;
+
+  while (leftIdx <= rightIdx) {
+    let midIdx = Math.floor((rightIdx + leftIdx) / 2);
+
+    if (arr[midIdx] === 1 && arr[midIdx + 1] === 1) leftIdx = midIdx + 1;
+    else if (arr[midIdx] === 0) rightIdx = midIdx - 1;
+    else return arr.length - midIdx - 1;
+  }
+  //return length of zeroes to the right; arr.length - idx - 1; ...ex: [0,0,0]
+
+  return 0;
 }
 
-module.exports = countZeroes
+module.exports = countZeroes;
--- sorted-frequency.js
+++ sorted-frequency.js
@@ -1,5 +1,43 @@
-function sortedFrequency() {
+function sortedFrequency(arr, num) {
+  console.log("sortedFrequency running");
+  let leftBd = 0,
+    rightBd = arr.length - 1; //set valid search window / bounds
 
+  if (num < leftBd || num > rightBd) return 0; //since it's sorted, return 0 if the num is out of bds
+
+  let leftIdx, rightIdx; //instantiate the two indices we are looking for
+
+  while (leftBd <= rightBd) {
+    let pointer = Math.floor((leftBd + rightBd) / 2); //set pointer to center of bounds
+    //if pointer is less than num look right
+    if (arr[pointer] < num) leftBd = pointer + 1;
+    //else if pointer is more than num look left
+    else if (arr[pointer] > num) rightBd = pointer - 1;
+    //else if pointer is num and has a !num to the left of it THAT is our leftIdx
+    else if (arr[pointer] === num && arr[pointer - 1] !== num)
+      leftIdx = pointer;
+    //else look left again
+    else rightBd = pointer - 1;
+  }
+  //search that searches right to find target (target num with !targetNum to the right)
+  while (leftBd <= rightBd) {
+    let pointer = Math.floor((leftBd + rightBd) / 2); //set pointer to center of bounds
+    //if pointer is less than num look right
+    if (arr[pointer] < num) leftBd = pointer + 1;
+    //else if pointer is more than num look left
+    else if (arr[pointer] > num) rightBd = pointer - 1;
+    //else if pointer is num and has a !num to the right of it THAT is our rightIdx
+    else if (arr[pointer] === num && arr[pointer + 1] !== num)
+      rightIdx = pointer;
+    //else look right again
+    else leftBd = pointer + 1;
+  }
+  //subtract the small index from the large
+  return rightIdx - leftIdx + 1;
 }
 
-module.exports = sortedFrequency
+module.exports = sortedFrequency;
+
+//[1, 1, 2, 2, 2, 2, 3]
+
+//think about refactoring only after it works
