62b0cd5584de030208da04ef404e94ab2b08a985
mits <dmitrybaycharov@gmail.com>
2021-08-16 12:35:06 -0500

    no debugger

--- sorted-frequency.test.js
+++ sorted-frequency.test.js
@@ -2,7 +2,6 @@ const sortedFrequency = require("./sorted-frequency");
 
 describe("#sortedFrequency", function () {
   it("returns the frequency", function () {
-    debugger;
     expect(sortedFrequency([1, 1, 2, 2, 2, 2, 3], 2)).toBe(4);
     expect(sortedFrequency([1, 1, 2, 2, 2, 2, 3], 3)).toBe(1);
     expect(sortedFrequency([1, 1, 2, 2, 2, 2, 3], 1)).toBe(2);
