d73713d48dac857a059080b4f309944c1f22d618
mits <dmitrybaycharov@gmail.com>
2021-08-16 15:25:26 -0500

    rotidx

--- find-rotated-index.js
+++ find-rotated-index.js
@@ -1,5 +1,8 @@
 function findRotatedIndex() {
- 
+  //binary search to find "wraparound" point
+  //cut array in half and glue it back together correctly
+  //binary search for num
+  //return index (recutting and regluing array into original order)
 }
 
-module.exports = findRotatedIndex
+module.exports = findRotatedIndex;
