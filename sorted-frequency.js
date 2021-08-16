39f1a16f0351c3b771ccddaeedff0ea122544944
mits <dmitrybaycharov@gmail.com>
2021-08-16 14:23:10 -0500

    sorted freq done

--- sorted-frequency.js
+++ sorted-frequency.js
@@ -1,46 +1,47 @@
 function sortedFrequency(arr, num) {
-  let leftBd = 0,
-    rightBd = arr.length - 1; //set valid search window / bounds
+  let boundL = 0,
+    boundR = arr.length - 1; //set valid search bounds
 
-  if (num < leftBd || num > rightBd) return 0; //since it's sorted, return 0 if the num is out of bds
+  if (num < arr[0] || num > arr[boundR]) return -1; //since it's sorted, return 0 if the num is out of bds
 
-  let leftIdx, rightIdx; //instantiate the two indices we are looking for
+  let pointer, targetL, targetR; //instantiate pointer and the two indices we are looking for
 
-  while (leftBd <= rightBd) {
-    let pointer = Math.floor((leftBd + rightBd) / 2); //set pointer to center of bounds
+  while (boundL <= boundR) {
+    pointer = Math.floor((boundL + boundR) / 2); //set pointer to center of bounds
     //if pointer is less than num look right
-    if (arr[pointer] < num) leftBd = pointer + 1;
+    if (arr[pointer] < num) boundL = pointer + 1;
     //else if pointer is more than num look left
-    else if (arr[pointer] > num) rightBd = pointer - 1;
-    //else if pointer is num and has a !num to the left of it THAT is our leftIdx
+    else if (arr[pointer] > num) boundR = pointer - 1;
+    //else if pointer is num and has a !num to the left of it THAT is our targetL
     else if (arr[pointer] === num && arr[pointer - 1] !== num) {
-      leftIdx = pointer;
+      targetL = pointer;
       break;
     }
     //else look left again
-    else rightBd = pointer - 1;
+    else boundR = pointer - 1;
   }
+
+  (boundL = 0), (boundR = arr.length - 1); //reset valid search window / bounds
+
   //search that searches right to find target (target num with !targetNum to the right)
-  while (leftBd <= rightBd) {
-    let pointer = Math.floor((leftBd + rightBd) / 2); //set pointer to center of bounds
+  while (boundL <= boundR) {
+    pointer = Math.floor((boundL + boundR) / 2); //set pointer to center of bounds
     //if pointer is less than num look right
-    if (arr[pointer] < num) leftBd = pointer + 1;
+    if (arr[pointer] < num) boundL = pointer + 1;
     //else if pointer is more than num look left
-    else if (arr[pointer] > num) rightBd = pointer - 1;
-    //else if pointer is num and has a !num to the right of it THAT is our rightIdx
+    else if (arr[pointer] > num) boundR = pointer - 1;
+    //else if pointer is num and has a !num to the right of it THAT is our targetR
     else if (arr[pointer] === num && arr[pointer + 1] !== num) {
-      rightIdx = pointer;
+      targetR = pointer;
       break;
     }
     //else look right again
-    else leftBd = pointer + 1;
+    else boundL = pointer + 1;
   }
   //subtract the small index from the large
-  return rightIdx - leftIdx + 1;
+  return targetR - targetL + 1;
 }
 
 module.exports = sortedFrequency;
 
 //[1, 1, 2, 2, 2, 2, 3]
-
-//think about refactoring only after it works
